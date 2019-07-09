e2c4b1f9fb27d17af713ab6eded593b517d134e3
Vladislav Unchuris <vlad.unchuris@gmail.com>
2019-07-09 17:30:42 +0600

    Initial commit 🚀

--- src/main/java/effective/com/lib/registry/CustomLintRegistry.kt
+++ src/main/java/effective/com/lib/registry/CustomLintRegistry.kt
@@ -3,11 +3,8 @@ package effective.com.lib.registry
 import com.android.tools.lint.client.api.IssueRegistry
 import effective.com.lib.issue.LayoutNameIssue
 import effective.com.lib.issue.XmlIdIssue
-import effective.com.lib.methodsOrder.issue.MethodsOrderIssue
 
 class CustomLintRegistry : IssueRegistry() {
 
-    override val issues = listOf(
-        XmlIdIssue.ISSUE, LayoutNameIssue.ISSUE, MethodsOrderIssue.ISSUE
-    )
+    override val issues = listOf(XmlIdIssue.ISSUE, LayoutNameIssue.ISSUE)
 }
